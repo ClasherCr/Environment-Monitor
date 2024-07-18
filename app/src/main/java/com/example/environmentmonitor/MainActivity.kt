0bd61fcad7c200638dde735e8e81e43ed6638887
Jashkumar Solanki <jashkumarsolanki@gmail.com>
2024-07-18 12:17:24 +0530

    About Us

--- app/src/main/java/com/example/environmentmonitor/MainActivity.kt
+++ app/src/main/java/com/example/environmentmonitor/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.example.environmentmonitor
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.environmentmonitor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
